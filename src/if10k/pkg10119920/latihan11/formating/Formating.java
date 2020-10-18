49314b89fdcde857502f1cb87062cb51bd155097
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:18:59 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan11/formating/Formating.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan11.formating;
+
+
+/**
+*
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Formating{
+    
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+
+
+}
